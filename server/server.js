5c24a8011547d786995a79e3f45423235588c368
kaveenakash <kaveenakashya@gmail.com>
2021-02-13 02:55:12 +0530

    lodash added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  
+
+  //lodash
+  const num =  _.random(0,20);
+  console.log(num)
 
   //Set header content type
   res.setHeader("content-type", "text/html");
 
-  let path = "../views/";
+  let path = "./views/";
 
   switch (req.url) {
     case "/":
@@ -18,7 +23,7 @@ const server = http.createServer((req, res) => {
       path += "about.html";
       res.statusCode = 200;
       break;
-    case "/about-me":
+    case "/about-us":
       res.statusCode = 301;
       res.setHeader("Location", "/about");
       res.end();
