083bed97c633afcd94b7e68b69a4ea06db3ca329
kaveenakash <kaveenakashya@gmail.com>
2021-02-13 09:29:53 +0530

    Express send file

--- server/app.js
+++ server/app.js
@@ -5,5 +5,9 @@ const app = express();
 app.listen(3000);
 
 app.get('/',(req,res) =>{
-    res.send('<p>Home page</p>')
+    res.sendFile('./views/index.html',{root:__dirname})
+})
+
+app.get('/about',(req,res) =>{
+    res.sendFile('./views/about.html',{root:__dirname})
 })
