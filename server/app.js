308db45d38fa18ee826619035c4717945faa34b4
kaveenakash <kaveenakashya@gmail.com>
2021-02-13 09:33:59 +0530

    express redirect

--- server/app.js
+++ server/app.js
@@ -11,3 +11,7 @@ app.get('/',(req,res) =>{
 app.get('/about',(req,res) =>{
     res.sendFile('./views/about.html',{root:__dirname})
 })
+
+app.get('/about-us',(req,res) =>{
+    res.redirect('/about');
+})
