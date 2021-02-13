fa433c19bb2c51649fb928651b31c22903d59d04
kaveenakash <kaveenakashya@gmail.com>
2021-02-13 08:50:28 +0530

    express js get data

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express();
+
+app.listen(3000);
+
+app.get('/',(req,res) =>{
+    res.send('<p>Home page</p>')
+})
--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 const http = require("http");
 const fs = require("fs");
-const _ = require('lodash');
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
-  
-
   //lodash
-  const num =  _.random(0,20);
-  console.log(num)
+  const num = _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log("hello");
+  });
+
+  greet();
+  greet();
 
   //Set header content type
   res.setHeader("content-type", "text/html");
